2d9ad80437bdf0fcebea9ae08b388686a475bf90
Kevin Garcia Martin <kevingm@live.se>
2025-04-11 12:20:34 +0200

    Add time and date format functions

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,2 +1,26 @@
 export const stringOuterProduct = (vec1, vec2) =>
   vec1.flatMap((string1) => vec2.map((string2) => `${string2}${string1}`));
+
+export const formatTimer = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+
+  const formattedMinutes = minutes.toString().padStart(2, '0');
+  const formattedSeconds = remainingSeconds.toString().padStart(2, '0');
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+export const formatDate = (dateString) => {
+  const date = new Date(dateString);
+
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
+}
+
