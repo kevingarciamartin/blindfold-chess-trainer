76fe49db5e8c2338260ad5acf50dd067d95aba42
Kevin Garcia Martin <kevingm@live.se>
2025-04-14 10:18:19 +0200

    Add scheduled highscore deletion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NotificationProvider } from "./contexts/NotificationContext";
+import { MidnightTaskProvider } from "./contexts/MidnightTaskContext";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home";
 import Highscores from "./components/Highscores/Highscores";
@@ -7,15 +8,17 @@ import Highscores from "./components/Highscores/Highscores";
 function App() {
   return (
     <NotificationProvider>
-      <BrowserRouter>
-        <Header />
-        <main className="pages">
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/highscores" element={<Highscores />}></Route>
-          </Routes>
-        </main>
-      </BrowserRouter>
+      <MidnightTaskProvider>
+        <BrowserRouter>
+          <Header />
+          <main className="pages">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/highscores" element={<Highscores />}></Route>
+            </Routes>
+          </main>
+        </BrowserRouter>
+      </MidnightTaskProvider>
     </NotificationProvider>
   );
 }
