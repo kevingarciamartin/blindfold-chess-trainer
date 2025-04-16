26ad7092c86a468b62f1aaef7e6184bef12fbbc4
Kevin Garcia Martin <kevingm@live.se>
2025-04-16 09:48:43 +0200

    Implement Google Analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ReactGA from "react-ga4";
 
-createRoot(document.getElementById('root')).render(
+ReactGA.initialize("G-P08JN6L0CF");
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
